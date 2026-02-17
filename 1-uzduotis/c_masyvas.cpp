60087a2623dc97d690a3a355c1ca323627ec980f
Jakub <jakub.r002@gmail.com>
2026-02-17 12:03:47 +0200

    Patuobulintas masyvas

--- 1-uzduotis/c_masyvas.cpp
+++ 1-uzduotis/c_masyvas.cpp
@@ -1,8 +1,9 @@
 #include <string>
-#include <vector>
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 using std::cin;
 using std::cout;
@@ -14,7 +15,6 @@ using std::setprecision;
 using std::setw;
 using std::sort;
 using std::string;
-using std::vector;
 
 const string vardai[] = {"Jonas", "Mantas", "Tomas", "Lukas", "Karolis", "Darius", "Paulius", "Mindaugas", "Justas", "Rokas", "Agne", "Ieva", "Egle", "Gabija", "Monika", "Karolina", "Viktorija", "Emilija", "Justina", "Greta"};
 
@@ -22,31 +22,42 @@ const string vyr_pavardes[] = {"Kazlauskas", "Jankauskas", "Petrauskas", "Stanke
 
 const string mot_pavardes[] = {"Kazlauskiene", "Jankauskiene", "Petrauskiene", "Stankeviciene", "Zukauskiene", "Butkiene", "Pociene", "Urboniene", "Mockiene", "Savickiene"};
 
-const int N = 100; // maks paz ir studentu kiekis
-
 struct Studentas
 {
     string vardas = "A", pavarde = "BB";
-    double C[N];
+    double *C = nullptr;
+    int talpa = 0;
+    int kiek = 0;
+
     int egz;
     double vid, med;
 };
 
-void inputas(Studentas grupe[], int &n);
-void outputas(Studentas grupe[], int n);
-void MedVidSkaciavimas(Studentas grupe[], int n, int length, int sum);
+void inputas(Studentas *&grupe, int &n, int &talpaS);
+void outputas(Studentas *grupe, int n);
+void MedVidSkaciavimas(Studentas *grupe, int n, int length, int sum);
 void randomVardasPavarde(string &vardas, string &pavarde);
+
+void UztikrintiVietosPazymiams(Studentas &grupe, int reikalinga);
+void UztikrintiVietosStudentams(Studentas *&grupe, int &talpaS, int reikalinga);
 
 int main()
 {
-    Studentas grupe[N];
+    Studentas *grupe = nullptr;
+
+    int talpaS = 0;
     int n;
-    inputas(grupe, n);
+    inputas(grupe, n, talpaS);
     outputas(grupe, n);
+    //atlaisviname atmintyje uzimta vieta
+    for (int i = 0; i < n; i++)
+        delete[] grupe[i].C;
+    delete[] grupe;
+
     return 0;
 }
 
-void outputas(Studentas grupe[], int n)
+void outputas(Studentas *grupe, int n)
 {
     int temp;
     cout << "Pasirinkite norima buda isvesti duomenis" << endl;
@@ -70,14 +81,14 @@ void outputas(Studentas grupe[], int n)
     }
     if (temp == 3)
     {
-        cout<< right << setw(20) << "Galutinis (Vid.)" << right << setw(20) << "Galutinis (Med.)" << endl;
+        cout << right << setw(20) << "Galutinis (Vid.)" << right << setw(20) << "Galutinis (Med.)" << endl;
         cout << left << setw(10) << "----------------------------------------------------------------------" << endl;
         for (int i = 0; i < n; i++)
             cout << left << setw(10) << grupe[i].vardas << left << setw(20) << grupe[i].pavarde << right << setw(20) << fixed << setprecision(2) << grupe[i].vid << right << setw(20) << grupe[i].med << endl;
     }
 }
 
-void inputas(Studentas grupe[], int &n)
+void inputas(Studentas *&grupe, int &n, int &talpaS)
 {
     srand(time(NULL));
     n = 0;
@@ -91,6 +102,7 @@ void inputas(Studentas grupe[], int &n)
         {
             while (true)
             {
+                UztikrintiVietosStudentams(grupe, talpaS, n + 1);
                 cout << "Iveskite 0, kad baigti ivedinet duomenis" << endl;
                 cout << "Iveskite varda ir pavarde: ";
                 cin >> grupe[n].vardas;
@@ -101,32 +113,29 @@ void inputas(Studentas grupe[], int &n)
                 cout << "Iveskite semestro ivercius. Kai baigsite iveskite 0" << endl;
                 int sum = 0;
                 int temp = -2;
-                int i = 0;
+                grupe[n].kiek = 0;
                 while (true)
                 {
-                    cout << "Iveskite " << i + 1 << " pazymi: ";
+                    cout << "Iveskite " << grupe[n].kiek + 1 << " pazymi: ";
                     cin >> temp;
                     if (temp == 0)
                         break;
-                    grupe[n].C[i] = temp;
+                    UztikrintiVietosPazymiams(grupe[n], grupe[n].kiek + 1);
+                    grupe[n].C[grupe[n].kiek] = temp;
                     sum += temp;
-                    i++;
+                    grupe[n].kiek++;
                 }
                 cout << "Iveskite egzamino invertinima: ";
-                cin >> grupe[N].egz;
-                MedVidSkaciavimas(grupe, n, i, sum);
+                cin >> grupe[n].egz;
+                MedVidSkaciavimas(grupe, n, grupe[n].kiek, sum);
                 n++;
-                if (n >= N)
-                {
-                    cout << "Pasiektas maksimalus studentu skaicius" << endl;
-                    break;
-                }
             }
         }
         if (t == 2)
         {
             while (true)
             {
+                UztikrintiVietosStudentams(grupe, talpaS, n + 1);
                 cout << "Iveskite 0, kad baigti ivedinet duomenis" << endl;
                 cout << "Iveskite varda ir pavarde: ";
                 cin >> grupe[n].vardas;
@@ -136,49 +145,47 @@ void inputas(Studentas grupe[], int &n)
                 int sum = 0,
                     rand_paz,
                     length = 10;
+                grupe[n].kiek = 0;
                 for (int i = 1; i <= length; i++)
                 {
                     rand_paz = rand() % 10 + 1;
-                    grupe[n].C[i - 1] = rand_paz;
-                    cout << "Sugeneruotas " << i << " pazymys: " << grupe[n].C[i - 1] << endl;
+                    UztikrintiVietosPazymiams(grupe[n], grupe[n].kiek + 1);
+                    grupe[n].C[grupe[n].kiek] = rand_paz;
+                    cout << "Sugeneruotas " << i << " pazymys: " << grupe[n].C[grupe[n].kiek] << endl;
                     sum += rand_paz;
+                    grupe[n].kiek++;
                 }
-                rand_paz = rand() % 10 + 1;
-                cout << "Sugeneruotas egzamino invertinimas: " << rand_paz << endl;
+                grupe[n].egz = rand() % 10 + 1;
+                cout << "Sugeneruotas egzamino invertinimas: " << grupe[n].egz << endl;
                 MedVidSkaciavimas(grupe, n, length, sum);
                 n++;
-                if (n >= N)
-                {
-                    cout << "Pasiektas maksimalus studentu skaicius" << endl;
-                    break;
-                }
             }
         }
         if (t == 3)
         {
             for (int i = 1; i <= 10; i++)
             {
+                UztikrintiVietosStudentams(grupe, talpaS, n + 1);
                 randomVardasPavarde(grupe[n].vardas, grupe[n].pavarde);
                 cout << "Sugeneruotas " << i << " vardas ir pavarde: " << grupe[n].vardas << " " << grupe[n].pavarde << endl;
                 int sum = 0,
                     rand_paz,
                     length = 10;
+                grupe[n].kiek = 0;
                 for (int ii = 1; ii <= length; ii++)
                 {
                     rand_paz = rand() % 10 + 1;
-                    grupe[n].C[ii - 1] = rand_paz;
-                    cout << "Sugeneruotas " << ii << " pazymys: " << grupe[n].C[i - 1] << endl;
+                    UztikrintiVietosPazymiams(grupe[n], grupe[n].kiek + 1);
+                    grupe[n].C[grupe[n].kiek] = rand_paz;
+                    cout << "test: " << rand_paz << endl;
+                    cout << "Sugeneruotas " << ii << " pazymys: " << grupe[n].C[grupe[n].kiek] << endl;
+                    grupe[n].kiek++;
                     sum += rand_paz;
                 }
                 grupe[n].egz = rand() % 10 + 1;
                 cout << "Sugeneruotas egzamino invertinimas: " << grupe[n].egz << endl;
                 MedVidSkaciavimas(grupe, n, length, sum);
                 n++;
-                if (n >= N)
-                {
-                    cout << "Pasiektas maksimalus studentu skaicius" << endl;
-                    break;
-                }
             }
         }
         if (t == 4)
@@ -186,7 +193,7 @@ void inputas(Studentas grupe[], int &n)
     }
 }
 
-void MedVidSkaciavimas(Studentas grupe[], int n, int length, int sum = 0)
+void MedVidSkaciavimas(Studentas *grupe, int n, int length, int sum = 0)
 {
 
     if (length == 0)
@@ -210,4 +217,47 @@ void randomVardasPavarde(string &vardas, string &pavarde)
         pavarde = vyr_pavardes[rand() % sizeof(vyr_pavardes) / sizeof(vyr_pavardes[0])];
     else
         pavarde = mot_pavardes[rand() % sizeof(mot_pavardes) / sizeof(mot_pavardes[0])];
+}
+
+// kad uztikrinti masyvo pletima, draugas pasiule ideja: pries irasant nauja reiksme patikriname, ar masyvas turi pakankamai vietos.
+// Jei vietos neuztenka, sukuriame nauja didesni masyva, perkeliame esamus duomenis, rodykle keiciame i nauja masyva ir sena istriname
+void UztikrintiVietosPazymiams(Studentas &grupe, int reikalinga)
+{
+    if (grupe.talpa >= reikalinga)
+        return;
+
+    int newTalpa = grupe.talpa;
+    if (newTalpa == 0)
+        newTalpa = 4; // didiname 4, kad daryti maziau veiksmu
+    while (newTalpa < reikalinga)
+        newTalpa = newTalpa * 2; // jeigu neuztenka, didiname dvigubai kad daryti maziau veiksmu
+
+    double *newC = new double[newTalpa];
+    for (int i = 0; i < grupe.kiek; i++)
+        newC[i] = grupe.C[i]; // perkeliame duomenis
+
+    //istriname sena masyva ir rodykle keiciame i nauja masyva
+    delete[] grupe.C;
+    grupe.C = newC;
+    grupe.talpa = newTalpa;
+}
+
+void UztikrintiVietosStudentams(Studentas *&grupe, int &talpaS, int reikalinga)
+{
+    if (talpaS >= reikalinga)
+        return;
+
+    int newTalpa = talpaS;
+    if (newTalpa == 0)
+        newTalpa = 4; // didiname 4, kad daryti maziau veiksmu
+    while (newTalpa < reikalinga)
+        newTalpa = newTalpa * 2; // jeigu neuztenka, didiname dvigubai kad daryti maziau veiksmu
+
+    Studentas *newGrupe = new Studentas[newTalpa];
+    for (int i = 0; i < reikalinga - 1; i++)
+        newGrupe[i] = grupe[i]; // perkeliame duomenis
+
+    delete[] grupe;
+    grupe = newGrupe;
+    talpaS = newTalpa;
 }
