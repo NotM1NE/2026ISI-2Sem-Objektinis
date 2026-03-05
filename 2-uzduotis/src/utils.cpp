1fccb99ea3ac4b995e840950a0ee3090dd1f4f40
Jakub <113280865+NotM1NE@users.noreply.github.com>
2026-03-06 00:57:10 +0200

    input throw

--- 2-uzduotis/src/utils.cpp
+++ 2-uzduotis/src/utils.cpp
@@ -3,41 +3,46 @@
 #include <limits>
 #include <cstdlib>
 
-using std::vector;
-using std::string;
-using std::cout;
 using std::cin;
+using std::cout;
 using std::numeric_limits;
-using std::streamsize;
 using std::rand;
+using std::streamsize;
+using std::string;
+using std::vector;
 
 const vector<string> vardai = {"Jonas", "Mantas", "Tomas", "Lukas", "Karolis", "Darius", "Paulius", "Mindaugas", "Justas", "Rokas",
-                                         "Agne", "Ieva", "Egle", "Gabija", "Monika", "Karolina", "Viktorija", "Emilija", "Justina", "Greta"};
+                               "Agne", "Ieva", "Egle", "Gabija", "Monika", "Karolina", "Viktorija", "Emilija", "Justina", "Greta"};
 
 const vector<string> vyr_pavardes = {"Kazlauskas", "Jankauskas", "Petrauskas", "Stankevicius", "Zukauskas", "Butkus", "Pocius", "Urbonas", "Mockus", "Savickas"};
 
 const vector<string> mot_pavardes = {"Kazlauskiene", "Jankauskiene", "Petrauskiene", "Stankeviciene", "Zukauskiene", "Butkiene", "Pociene", "Urboniene", "Mockiene", "Savickiene"};
 
-void intInput(int& temp)
+void intInput(int &temp)
 {
-    int input;
     while (true)
     {
-        if (cin >> input)
+        try
         {
-            temp = input;
+            if (!(cin >> temp))
+            {
+                throw std::invalid_argument("Netinkamas ivestis. Bandykite dar karta.");
+                
+            }
             return;
         }
-        else
+        catch (const std::exception &e)
         {
+            std::cerr << e.what() << '\n';
             cout << "Netinkamas ivestis. Bandykite dar karta.\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            
         }
     }
 }
 
-void randomVardasPavarde(string& vardas, string& pavarde)
+void randomVardasPavarde(string &vardas, string &pavarde)
 {
     vardas = vardai[rand() % vardai.size()];
     if (!vardas.empty() && vardas.back() == 's')
